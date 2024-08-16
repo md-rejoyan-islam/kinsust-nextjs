07ac89305eae60293e3af50ed862ca428bebd124
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-08-17 01:25:36 +0600

    loading page added

--- app/find-account/loading.js
+++ app/find-account/loading.js
@@ -2,7 +2,7 @@ import { ScaleLoader } from "react-spinners";
 
 export default function loading() {
   return (
-    <div className="top-0 left-0 z-[10] h-screen w-full mx-auto flex justify-center bg-white dark:bg-[#0f0826] overflow-hidden absolute items-center">
+    <div className="top-0 left-0 z-[10] h-screen w-full mx-auto flex justify-center bg-white dark:bg-[#0f0826]  absolute items-center">
       <div className="text-center">
         <ScaleLoader
           color="#36d7b7"
