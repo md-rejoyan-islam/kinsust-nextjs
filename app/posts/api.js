374f22a78dfde36f4d9d0010f47eea7fd83cb179
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-08-17 00:47:37 +0600

    update

--- app/posts/api.js
+++ app/posts/api.js
@@ -1,12 +1,20 @@
 export const allBlogs = async () => {
-  const response = await fetch(`${process.env.SERVER_URL}/api/v1/posts`);
+  const response = await fetch(
+    `${process.env.SERVER_URL}/api/v1/posts` <
+      {
+        cache: "no-cache",
+      }
+  );
   const result = await response.json();
   return result?.data || {};
 };
 
 export const singleBlog = async (slug) => {
   const response = await fetch(
-    `${process.env.SERVER_URL}/api/v1/posts/${slug}`
+    `${process.env.SERVER_URL}/api/v1/posts/${slug}`,
+    {
+      cache: "no-cache",
+    }
   );
   const result = await response.json();
   return result?.data || {};
